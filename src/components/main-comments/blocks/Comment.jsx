e573f81aa286a095f3325e5b2aeb8ab598b6d809
Crowberry <crowberry23@gmail.com>
2018-02-16 17:23:25 +0100

    opti comment component

--- src/components/main-comments/blocks/Comment.jsx
+++ src/components/main-comments/blocks/Comment.jsx
@@ -2,29 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Markdown from '../../Markdown';
 
-const Comment = ({ comment, direction }) => (
-  <div
-    className={`clearfix authorIssue clear ${direction}`}
-  >
-    <a
-      href={comment.user.html_url}
-      title={comment.user.login}
-      className={`avatar-user ${direction}`}
-    >
-      <img
-        alt={comment.user.login}
-        src={comment.user.avatar_url}
-      />
-    </a>
-    <Markdown className={`comment ${direction}`} >
-      {comment.body}
-    </Markdown>
-  </div>
-);
+const Comment = ({ comment, fromAuthor }) => {
+  const direction = fromAuthor ? 'pull-left' : 'pull-right';
+  return (
+    <div className={`clearfix authorIssue clear ${direction}`}>
+      <a
+        href={comment.user.html_url}
+        title={comment.user.login}
+        className={`avatar-user ${direction}`}
+      >
+        <img alt={comment.user.login} src={comment.user.avatar_url} />
+      </a>
+      <Markdown className={`comment ${direction}`}>{comment.body}</Markdown>
+    </div>
+  );
+};
 
 Comment.propTypes = {
   comment: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  direction: PropTypes.string.isRequired,
+  fromAuthor: PropTypes.bool.isRequired,
 };
 
 
